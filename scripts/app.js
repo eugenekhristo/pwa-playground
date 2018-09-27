957f5cd59030a3eac0dbe74711036f111a7759a0
Eugene Khristo <marteau9292@gmail.com>
2018-09-27 11:54:34 +0300

    Deffered prompt is added

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,24 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js');
 }
+
+let deferredPromt;
+
+window.addEventListener('beforeinstallprompt', e => {
+  e.preventDefault();
+  deferredPromt = e;
+});
+
+const $addPromptBtn = document.getElementById('addPropmtBtn');
+
+$addPromptBtn.addEventListener('click', e => {
+  if (deferredPromt) {
+    deferredPromt.prompt();
+
+    deferredPromt.userChoice.then(choiceResult => {
+      console.log('choiceResult - ', choiceResult);
+      console.log('choiceResult.outcome - ', choiceResult.outcome);
+      deferredPrompt = null;
+    });
+  }
+});
